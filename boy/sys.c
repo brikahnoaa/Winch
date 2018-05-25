43609ed6c429595cc3a31587a31a43b6c155e76e
Brian Kahn <brian.kahn@noaa.gov>
2018-05-24 17:20:25 -0700

    comments in sys.c re serial buf 64K

--- boy/sys.c
+++ boy/sys.c
@@ -31,14 +31,14 @@ IEV_C_PROTO(ExtFinishPulseRuptHandler);
 // uses: sys.starts
 int sysInit(void) {
   TUChParams *params;
-  short qsize = 16*BUFSZ;
+  short qsize = 64*1024;
   preRun(10);
   sys.starts = startCheck();
   dbgInit();              // common init: dbg0,1,2
   utlInit();              // malloc global utlStr
   logInit(sys.logFile);   // stores flogf filename, found in VEE.sys_log
   cfgInit();
-  // make serial queues larger (16K)
+  // make serial queues larger = 64K (rudics max block size)
   params = TUGetDefaultParams();
   params->rxqsz = qsize;
   params->txqsz = qsize;
