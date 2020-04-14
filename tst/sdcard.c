043e5406c7f5e840fa0f3f55e47f189e344a26f1
Brian Kahn <brian.kahn@noaa.gov>
2020-04-14 12:47:05 -0700

    sdcard test - size

---
+++ tst/sdcard.c
@@ -0,0 +1,45 @@
+// sdcard
+#include <test.h>
+
+extern BoyInfo boy;
+extern SysInfo sys;
+
+bool CFCardDetect(void);
+void CFEnable(bool on);
+void *CFGetDriver(void);
+short ATACapacity(ATADvr iodvr, ulong *sectors, ushort *spt, ushort *heads, void **info);
+short ATAReadSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+short ATAWriteSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+//
+
+void main(void){
+  int sdGB=32;
+  short ok;
+  ATADvr iodvr;
+  //
+  ulong *sectors;
+  ushort *spt;
+  ushort *heads;
+  void **info;
+  //
+  ulong sector;
+  void *buffer;
+  short count;
+  //
+  sysInit();
+  mpcInit();
+  iodvr=CFGetDriver(void);
+  //
+  cprintf("\n sdcard read direct access \n");
+  //
+  if (CFCardDetect()) {
+    CFEnable(true);
+    ok=ATACapacity(ATADvr iodvr, 
+        ulong *sectors, ushort *spt, ushort *heads, void **info);
+    cprintf("card has %ld sectors   (status %d)\n", *sectors, ok);
+  }
+  exit(0);
+  //
+  ok=ATAReadSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+  ok=ATAWriteSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+}
