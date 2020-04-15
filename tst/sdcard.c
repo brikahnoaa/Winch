9d8eb9b4320a51a3f0a3dc4b1ef52a2bb3e63edc
Brian Kahn <brian.kahn@noaa.gov>
2020-04-15 12:04:49 -0700

    corrected

--- tst/sdcard.c
+++ tst/sdcard.c
@@ -4,12 +4,12 @@
 extern BoyInfo boy;
 extern SysInfo sys;
 
-bool CFCardDetect(void);
-void CFEnable(bool on);
-void *CFGetDriver(void);
-short ATACapacity(ATADvr iodvr, ulong *sectors, ushort *spt, ushort *heads, void **info);
-short ATAReadSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
-short ATAWriteSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+// bool CFCardDetect(void);
+// void CFEnable(bool on);
+// void *CFGetDriver(void);
+// short ATACapacity(ATADvr iodvr, ulong *sectors, ushort *spt, ushort *heads, void **info);
+// short ATAReadSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
+// short ATAWriteSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
 //
 
 void main(void){
@@ -17,10 +17,10 @@ void main(void){
   short ok;
   ATADvr iodvr;
   //
-  ulong *sectors;
-  ushort *spt;
-  ushort *heads;
-  void **info;
+  ulong sectors;
+  ushort spt;
+  ushort heads;
+  void *info;
   //
   ulong sector;
   void *buffer;
@@ -28,18 +28,15 @@ void main(void){
   //
   sysInit();
   mpcInit();
-  iodvr=CFGetDriver(void);
+  iodvr=CFGetDriver();
   //
   cprintf("\n sdcard read direct access \n");
   //
   if (CFCardDetect()) {
     CFEnable(true);
-    ok=ATACapacity(ATADvr iodvr, 
-        ulong *sectors, ushort *spt, ushort *heads, void **info);
-    cprintf("card has %ld sectors   (status %d)\n", *sectors, ok);
+    ok=ATACapacity(iodvr, &sectors, &spt, &heads, &info);
+    cprintf("card has %ld sectors   (status %d)\n", sectors, ok);
   }
   exit(0);
   //
-  ok=ATAReadSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
-  ok=ATAWriteSectors(ATADvr iodvr, ulong sector, void *buffer, short count);
 }
