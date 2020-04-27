98d031268b93191c4e76b7438d610ed3c590341c
Brian Kahn <brian.kahn@noaa.gov>
2020-04-27 15:24:16 -0700

    wspCmd.c v0

---
+++ tst/wspCmd.c
@@ -0,0 +1,20 @@
+// wspCmd.c
+#include <test.h>
+
+
+void main(void){
+  int r;
+  sysInit();
+  mpcInit();
+  wspInit();
+  while (!(r=CIOgets(all.str, 100))) { // wait for input
+    if (all.str[0]=='q')
+      exit(0);
+  //
+  cprintf("\n wspr command: '%s'\n", all.str);
+  wspStart();
+  wspOpen(); // ready to accept command
+  utlWrite(wsp.port, all.str, EOL);
+  r=utlReadWait(wsp.port, all.buf, 44);
+  cprintf("\n%s\n", utlNonPrintBlock(all.buf, r));
+}
