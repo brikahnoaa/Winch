b04238a059a79d4bd32288a0068822ef7a185c9c
Brian Kahn <brian.kahn@noaa.gov>
2020-05-08 11:52:58 -0700

    PWRSuspendUntil simple test

--- tst/sleep.c
+++ tst/sleep.c
@@ -1,7 +1,30 @@
-// sleep
-#include <main.h>
+// sleep.c
+#include <test.h>
+
 void main(void){
-  cprintf("\n Low Power Sleep %s \n", utlDateTime());
-  cdrain();
-  utlSleep();
+  time_t now;
+  // char *buff;
+  // int i, j, r;
+  // int blk, len, fil, del;
+  // blk = 4; len = 1024; fil = 1; del = 0;
+  sysInit();
+  mpcInit();
+  //
+  //
+  cprintf("\nsleep 6 sec\n");
+  //if (dbg.t1) blk = dbg.t1;
+  //if (dbg.t2) len = dbg.t2;
+  //if (dbg.t3) fil = dbg.t3;
+  //if (dbg.t4) del = dbg.t4;
+  //cprintf("\n block.t1=%d   length.t2=%d   files.t3=%d   delay.t4=%d\n", 
+      //blk, len, fil, del);
+  //
+  time(&now);
+  cprintf("to sleep,");
+  PWRSuspendUntil(now+2, true, WakeOnTimeout);
+  cprintf(" perchance ");
+  PWRSuspendUntil(now+4, true, WakeOnTimeout);
+  cprintf("to dream");
+  PWRSuspendUntil(now+6, true, WakeOnTimeout);
+  exit(0);
 }
