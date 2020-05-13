3a3d111c0ebe9b42b2ec7207a6630e240df0f5ea
Brian Kahn <brian.kahn@noaa.gov>
2020-05-13 13:30:43 -0700

    serial works at 3640 but console falls apart below that down to 480 between 460 and 220 serial works well switching from medium to low speed can kill the RTClock, use hi to low

--- tst/speed.c
+++ tst/speed.c
@@ -1,44 +1,60 @@
 // speed.c
 #include <test.h>
 
+void idle(int sec);
+
 void main(void){
-  int i=0, best=14720, delay=3, divisor=4, speed; // vs 16000
-  // char *buff;
-  // int i, j, r;
-  // int blk, len, fil, del;
-  // blk = 4; len = 1024; fil = 1; del = 0;
+  int i, best=14720, good=3680, slow=1840, repeat=3, delay=3, speed; 
   sysInit();
   mpcInit();
   //
-  //
   cprintf("\nclock speeds\n");
-  if (dbg.t1) divisor = dbg.t1;
-  cprintf(" divisor.t1=%d  delay.t2=%d\n", 
-    divisor, delay);
-  //if (dbg.t2) len = dbg.t2;
-  //if (dbg.t3) fil = dbg.t3;
-  //if (dbg.t4) del = dbg.t4;
-  //cprintf("\n block.t1=%d   length.t2=%d   files.t3=%d   delay.t4=%d\n", 
-      //blk, len, fil, del);
+  if (dbg.t1) good = dbg.t1;
+  if (dbg.t2) slow = dbg.t2;
+  if (dbg.t3) repeat = dbg.t3;
+  if (dbg.t4) delay = dbg.t4;
+  cprintf(" good.t1=%d  slow.t2=%d  repeat.t3=%d  delay.t4=%d \n", 
+    good, slow, repeat, delay);
   //
   cprintf("%s\n", utlDateTime());
-  speed=best/(divisor);
+  speed=best;
   cprintf("[%5d] ", speed);
   TMGSetSpeed(speed);
   cprintf("to sleep, ");
   cprintf("%s\n", utlDateTime());
-  utlNap(delay);
-  speed=best/(divisor*2);
+  idle(delay);
+  speed=good;
   cprintf("[%5d] ", speed);
   TMGSetSpeed(speed);
   cprintf(" perchance ");
   cprintf("%s\n", utlDateTime());
-  utlNap(delay);
-  speed=best/(divisor*4);
-  cprintf("[%5d] ", speed);
-  TMGSetSpeed(speed);
-  cprintf("to dream ");
-  cprintf("%s\n", utlDateTime());
-  utlNap(delay);
-  BIOSResetToPicoDOS();
+  idle(delay);
+  for (i=0; i<repeat; i++) {
+    speed=slow;
+    cprintf("[%5d] ", speed);
+    TMGSetSpeed(speed);
+    cprintf("to dream ");
+    cprintf("%s\n", utlDateTime());
+    idle(delay);
+    speed=good;
+    cprintf("[%5d] ", speed);
+    TMGSetSpeed(speed);
+  }
+  exit(0);
+}
+
+void idle(int sec){
+  time_t now, then;
+  struct tm *tim;
+  time(&now); 
+  then=now+sec; 
+  while (now<then) {
+    time(&now); 
+    if (cgetq()) {
+      tim = gmtime(&now);
+      cprintf("\n %02d:%02d:%02d\n", 
+        tim->tm_hour, tim->tm_min, tim->tm_sec);
+      if (cgetc()=='q') exit(1);
+    }
+  }
 }
