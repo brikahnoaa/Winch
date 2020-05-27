21594b00d24df674987f8497e3a1fb4daca0fcb5
Brian Kahn <brian.kahn@noaa.gov>
2020-05-27 11:37:29 -0700

    pwrSleep add 1 and round up

--- tst/sleeplite.c
+++ tst/sleeplite.c
@@ -1,59 +1,62 @@
 // sleeplite.c
 #include <test.h>
 
-int dog;
-void MyChore(void); 
-void MyChore(void) { if (! --dog) utlErr( watchdog_err, "woof" ); } 
-static void Irq4RxISR(void);
-static void Irq4RxISR(void) { PinIO(IRQ4RXD); RTE(); }
+// int dog;
+// void MyChore(void); 
+// void MyChore(void) { if (! --dog) utlErr( watchdog_err, "woof" ); } 
+// static void Irq4RxISR(void);
+// static void Irq4RxISR(void) { PinIO(IRQ4RXD); RTE(); }
 
 void main(void){
   time_t now, then;
-  int cons=0, watchdog=30, sleepsec=6, tuning=27;
+  int sleepsec=6;
   sysInit();
   mpcInit();
+  pwrInit();
   //
-  if (dbg.t1) watchdog = dbg.t1;
+  // if (dbg.t1) watchdog = dbg.t1;
   if (dbg.t2) sleepsec = dbg.t2;
-  if (dbg.t3) tuning = dbg.t3;
+  // if (dbg.t3) tuning = dbg.t3;
   cprintf("\nsleep lite\n");
-  cprintf(" watchdog:t1=%d, sleepsec:t2=%d, tuning:t3=%d\n", 
-    watchdog, sleepsec, tuning);
+  cprintf(" sleepsec:t2=%d\n", sleepsec);
+  // cprintf(" watchdog:t1=%d, sleepsec:t2=%d, tuning:t3=%d\n", 
+    // watchdog, sleepsec, tuning);
  
-  sleepsec = 1.027 * sleepsec;
+  // sleepsec = 1.027 * sleepsec;
   // this goes in utlInit?
-  dog=watchdog;
-  IEVInsertAsmFunct(Irq4RxISR, level4InterruptAutovector);
-  IEVInsertAsmFunct(Irq4RxISR, spuriousInterrupt);
+  // dog=watchdog;
+  // IEVInsertAsmFunct(Irq4RxISR, level4InterruptAutovector);
+  // IEVInsertAsmFunct(Irq4RxISR, spuriousInterrupt);
   //
-  PITInit(6); //At priority 6  
-  PITSet51msPeriod(19);  // ~ 1sec 
-  PITAddChore(MyChore,6); 
+  // PITInit(6); //At priority 6  
+  // PITSet51msPeriod(19);  // ~ 1sec 
+  // PITAddChore(MyChore,6); 
   //
   time(&now);
   then = now;
   cprintf("sleep %d @ %s\n", sleepsec, utlDateTimeFmt(now));
   cdrain();
   utlDelay(100);
-  while(sleepsec--) 
-  { // loop until console BREAK or PIT seconds
-    PinBus(IRQ4RXD);
-    LPStopCSE(FullStop);
+  pwrSleep(sleepsec);
+  // while(sleepsec--) 
+  // { // loop until console BREAK or PIT seconds
+    // PinBus(IRQ4RXD);
+    // LPStopCSE(FullStop);
     // console or PIT?
-    if (!PinTestIsItBus(IRQ4RXD))
-      if (SCIRxBreak(50)) 
-      { // not noise
-        cons = 1;
+    // if (!PinTestIsItBus(IRQ4RXD))
+      // if (SCIRxBreak(50)) 
+      // { // not noise
+        // cons = 1;
         // consume BREAK
-        while (SCIRxBreak(10)) {}
-        break;
-      }
-  } // loop
+        // while (SCIRxBreak(10)) {}
+        // break;
+      // }
+  // } // loop
   //
-  if (cons)
-    cprintf("user break. sleepsec remaining=%d\n", sleepsec);
-  PITSet51msPeriod(PITOff);  
-  PITRemoveChore(MyChore); 
+  // if (cons)
+    // cprintf("user break. sleepsec remaining=%d\n", sleepsec);
+  // PITSet51msPeriod(PITOff);  
+  // PITRemoveChore(MyChore); 
   time(&now);
   cprintf("slept %ld @ %s\n", now-then, utlDateTimeFmt(now));
   cdrain();
